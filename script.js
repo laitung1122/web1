3a62d86ac7eda0738b4c1ef7158bc4014b7703dd
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-11-17 09:53:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const moveRandom = document.querySelector("#move-random");
 moveRandom.addEventListener("mouseenter", function (e) {
   moveRandomEl(e.target);
 });
+const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "fas fa-heart";
+    heart.style.left = (Math.random() * 100)+"vw";
+    heart.style.animationDuration = (Math.random()*3)+2+"s"
+    body.appendChild(heart);
+}
+setInterval(createHeart,100);
+setInterval(function name(params) {
+    var heartArr = document.querySelectorAll(".fa-heart")
+    if (heartArr.length > 200) {
+       heartArr[0].remove()
+    }
+    //console.log(heartArr);
+},100)
